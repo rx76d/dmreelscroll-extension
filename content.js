116595117e8e5ff2bcafe3dedcc19ed6722c29fe
rx76d <245590600+rx76d@users.noreply.github.com>
2026-02-19 22:43:33 +0530

    Added functionality by adding js

---
+++ content.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    const CONFIG = {
+        minWidth: 100,
+        minHeight: 140,
+        watchBuffer: 0.5,
+        scrollDelay: 1000,
+        photoDuration: 3,
+        maxLoadWait:5000
+    };
+
+    let isRunning = false;
+    let playDirection ='down';
+    
+})
